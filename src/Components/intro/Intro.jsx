abfcc6d0f231a2ae99171a5b827c951bada41a1e
deepak <deepak21devops@gmail.com>
2021-09-24 13:51:17 +0530

    intro final

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './intro.scss'
 import { ArrowDownward } from '@material-ui/icons'
+import { init } from 'ityped'
+
+
 const Intro = () => {
+
+    const textRef = useRef()
+
+
+    useEffect(() => {
+        init(textRef.current,
+            {
+                showCursor: true,
+                // backDelay: 600,
+                backSpeed: 60,
+                strings: ['Developer', 'Designer', 'Creator']
+            })
+    }, [])
     return (
         <div className="intro" id="home">
             <div className="left">
@@ -13,7 +29,7 @@ const Intro = () => {
                 <div className="wrapper">
                     <h1>Hi There, I'm</h1>
                     <h2>Deepak Kumar Bakki</h2>
-                    <h3>Developer</h3>
+                    <h3>Application <span ref={textRef}></span></h3>
 
                 </div>
                 <div className="arrow">
