4545869fe72dcc4c9b77d5bb1f648c8d08213290
deepak <deepak21devops@gmail.com>
2021-09-24 09:07:33 +0530

    intro Page Done

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './intro.scss'
-
+import { ArrowDownward } from '@material-ui/icons'
 const Intro = () => {
     return (
-        <div className="intro-container" id="home">
-            INtro
+        <div className="intro" id="home">
+            <div className="left">
+                <div className="imageContainer">
+                    <img src="assets/man.png" alt="man"></img>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h1>Hi There, I'm</h1>
+                    <h2>Deepak Kumar Bakki</h2>
+                    <h3>Developer</h3>
+
+                </div>
+                <div className="arrow">
+                    <a href="#portfolio"><ArrowDownward class="icon" /></a>
+                </div>
+
+            </div>
         </div>
     )
 }
