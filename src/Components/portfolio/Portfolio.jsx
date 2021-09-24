8a2f4c0a6c2de9ae50664700ca76a3dd036acc05
deepak <deepak21devops@gmail.com>
2021-09-24 18:25:35 +0530

    portfolio page

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -4,7 +4,51 @@ import './portfolio.scss'
 const Portfolio = () => {
     return (
         <div className="portfolio" id="portfolio">
-            Portfolio
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/web_3.jpg" alt="web"></img>
+                    <h1>Banking Application</h1>
+                </div>
+
+                <div className="item">
+                    <img src="assets/web_4.png" alt="web"></img>
+                    <h1>Banking Application</h1>
+                </div>
+                <div className="item">
+                    <img src="assets/web_5.png" alt="web"></img>
+                    <h1>Banking Application</h1>
+                </div>
+                <div className="item">
+                    <img src="assets/web_6.jpg" alt="web"></img>
+                    <h1>Banking Application</h1>
+                </div>
+                <div className="item">
+                    <img src="assets/web_7.jpg" alt="web"></img>
+                    <h1>Banking Application</h1>
+                </div>
+                <div className="item">
+                    <img src="assets/web_8.jpeg" alt="web"></img>
+                    <h1>Banking Application</h1>
+                </div>
+                <div className="item">
+                    <img src="assets/web_9.jpg" alt="web"></img>
+                    <h1>Banking Application</h1>
+                </div>
+                <div className="item">
+                    <img src="assets/web.jpeg" alt="web"></img>
+                    <h1>Banking Application</h1>
+                </div>
+
+
+            </div>
         </div>
     )
 }
