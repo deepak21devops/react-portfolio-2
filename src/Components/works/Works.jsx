3eb8757efde90c9ba14f75c792ec5ac895c1ff98
deepak <deepak21devops@gmail.com>
2021-09-27 07:10:33 +0530

    slider complete

--- src/Components/works/Works.jsx
+++ src/Components/works/Works.jsx
@@ -1,29 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './works.scss'
-import { PhoneAndroid } from '@material-ui/icons'
+import { PhoneAndroid, ArrowBackIos } from '@material-ui/icons'
+import { slideList } from './slides.js'
 
 const Works = () => {
+
+    const [toggleSlide, setToggleSlide] = useState(0)
+
+    const handleClick = (toggle) => {
+        toggle === "left" ? setToggleSlide(toggleSlide > 0 ? toggleSlide - 1 : 2)
+            : setToggleSlide(toggleSlide < slideList.length - 1 ? toggleSlide + 1 : 0)
+    }
     return (
         <div className="works" id="works">
-            <div className="slider">
-                <div className="container">
-                    <div className="item">
-                        <div className="left">
-                            <div className="leftContainer">
-                                <div className="imageContainer">
-                                    <PhoneAndroid className="imgicon"/>
+            <div className="slider" style={{ transform: `translateX(-${toggleSlide * 100}vw)` }}>
+                {
+                    slideList.map(itm => (
+                        <div className="container">
+                            <div className="item">
+                                <div className="left">
+                                    <div className="leftContainer">
+                                        <div className="imageContainer">
+                                            <PhoneAndroid className="imgicon" />
+                                        </div>
+                                        <h1>{itm.title}</h1>
+                                        <p>{itm.desc}
+                                        </p>
+                                        <span>{itm.proj}</span>
+                                    </div>
                                 </div>
-                                <h1>Title</h1>
-                                <p>We have what we want but we can't get what we need.
-                                    We have what we want but we can't get what we need
-                                </p>
-                                <span>Projects</span>
+                                <div className="right">
+                                    <img src={`assets/${itm.img}`} alt="img"></img>
+                                </div>
                             </div>
+
                         </div>
-                        <div className="right"></div>
-                    </div>
-                </div>
+
+                    ))
+                }
+
+
+
             </div>
+            <ArrowBackIos className="arrow leftarrow" onClick={() => handleClick("left")} />
+            <ArrowBackIos className="arrow rightarrow" onClick={() => handleClick()} />
         </div>
     )
 }
