1fccace6fa576336c47b284835ec4b30245b387a
deepak <deepak21devops@gmail.com>
2021-10-01 19:37:25 +0530

    responsive final

--- src/Components/works/slides.js
+++ src/Components/works/slides.js
@@ -3,7 +3,7 @@ export const slideList = [
         id: 1,
         img: "slid_1.png",
         title: "Web Development",
-        desc: "We have what we want but we can't get what we need.We have what we want but we can't get what we need",
+        desc: "We have what we want but we can't get what we need.",
         proj: "Web Project"
 
     },
@@ -11,7 +11,7 @@ export const slideList = [
         id: 2,
         img: "slid_2.png",
         title: "Mobile Development",
-        desc: "We have what we want but we can't get what we need.We have what we want but we can't get what we need",
+        desc: "We have what we want but we can't get what we need.",
         proj: "Mobile Project"
 
     },
@@ -19,7 +19,7 @@ export const slideList = [
         id: 3,
         img: "slider_1.jpg",
         title: "Hybrid Projects",
-        desc: "We have what we want but we can't get what we need.We have what we want but we can't get what we need",
+        desc: "We have what we want but we can't get what we need.",
         proj: "Hybrid Project"
 
     }
